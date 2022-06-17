0feddf8eecc217485a8ab5794437aa5c070516a2
FallenGod <danil.sidorov16@gmail.com>
2022-06-17 12:59:07 +0300

    Output and worker items

--- Assets/Scripts/Components/Items/OutputComponent.cs
+++ Assets/Scripts/Components/Items/OutputComponent.cs
@@ -1,18 +1,92 @@
 using System.Collections;
 using System.Collections.Generic;
+using TMPro;
 using UnityEngine;
+using UnityEngine.UI;
 
-public class OutputComponent : MonoBehaviour
+public class OutputComponent : ItemComponent
 {
-    // Start is called before the first frame update
-    void Start()
+    [SerializeField] private TMP_InputField CountField;
+    [SerializeField] private TMP_InputField PriceField;
+    [SerializeField] private Slider PrioritySlider;
+    [SerializeField] private TextMeshProUGUI PriorityLabel;
+
+    public float Count;
+    public float Price;
+    public float Priority;
+
+    private float countUnsaved;
+    private float priceUnsaved;
+    private float priorityUnsaved;
+
+    public override void Start()
     {
-        
+        priorityUnsaved = 50;
+        base.Start();
     }
 
-    // Update is called once per frame
-    void Update()
+    // Changing increase value
+    public void ChangeCount(string count)
     {
-        
+        if (float.TryParse(count, out float result))
+        {
+            countUnsaved = result;
+        }
+        else
+        {
+            CountField.text = countUnsaved.ToString();
+            Debug.LogError("Incorrect Count");
+        }
+    }
+
+    // Changing limit
+    public void ChangePrice(string price)
+    {
+        if (float.TryParse(price, out float result))
+        {
+            priceUnsaved = result;
+        }
+        else
+        {
+            PriceField.text = priceUnsaved.ToString();
+            Debug.LogError("Incorrect Price");
+        }
+    }
+
+    // Changing priority
+    public void ChangePriority(float priority)
+    {
+        priorityUnsaved = Mathf.Round(priority);
+        PriorityLabel.text = $"{priorityUnsaved}%";
+    }
+
+    // Save
+    public override void Save()
+    {
+        Count = countUnsaved;
+        Price = priceUnsaved;
+        Priority = priorityUnsaved;
+        base.Save();
+        UpdateFieldValues();
+        DisactivaveUI();
+    }
+
+    // Abort
+    public override void Abort()
+    {
+        countUnsaved = Count;
+        priceUnsaved = Price;
+        priorityUnsaved = Priority;
+        base.Abort();
+        UpdateFieldValues();
+        DisactivaveUI();
+    }
+
+    // Change fields
+    public void UpdateFieldValues()
+    {
+        CountField.text = Count.ToString();
+        PriceField.text = Price.ToString();
+        PrioritySlider.value = priorityUnsaved;
     }
 }
--- Assets/Scripts/Components/Items/WorkerComponent.cs
+++ Assets/Scripts/Components/Items/WorkerComponent.cs
@@ -1,18 +1,92 @@
 using System.Collections;
 using System.Collections.Generic;
+using TMPro;
 using UnityEngine;
+using UnityEngine.UI;
 
-public class WorkerComponent : MonoBehaviour
+public class WorkerComponent : ItemComponent
 {
-    // Start is called before the first frame update
-    void Start()
+    [SerializeField] private TMP_InputField PerformanceField;
+    [SerializeField] private TMP_InputField WorkField;
+    [SerializeField] private TMP_InputField RelaxField;
+
+    public float Performance;
+    public float WorkTime;
+    public float RelaxTime;
+
+    private float performanceUnsaved;
+    private float workTimeUnsaved;
+    private float relaxTimeUnsaved;
+
+    // Changing performance value
+    public void ChangePerformance(string performance)
     {
-        
+        if (float.TryParse(performance, out float result))
+        {
+            performanceUnsaved = result;
+        }
+        else
+        {
+            PerformanceField.text = performanceUnsaved.ToString();
+            Debug.LogError("Incorrect Performance Value");
+        }
     }
 
-    // Update is called once per frame
-    void Update()
+    // Changing work time
+    public void ChangeWorkTime(string workTime)
     {
-        
+        if (float.TryParse(workTime, out float result))
+        {
+            workTimeUnsaved = result;
+        }
+        else
+        {
+            WorkField.text = workTimeUnsaved.ToString();
+            Debug.LogError("Incorrect Work Time");
+        }
+    }
+
+    // Changing relax time
+    public void ChangeRelaxTime(string relaxTime)
+    {
+        if (float.TryParse(relaxTime, out float result))
+        {
+            relaxTimeUnsaved = result;
+        }
+        else
+        {
+            RelaxField.text = relaxTimeUnsaved.ToString();
+            Debug.LogError("Incorrect Relax Time");
+        }
+    }
+
+    // Save
+    public override void Save()
+    {
+        Performance = performanceUnsaved;
+        WorkTime = workTimeUnsaved;
+        RelaxTime = relaxTimeUnsaved;
+        base.Save();
+        UpdateFieldValues();
+        DisactivaveUI();
+    }
+
+    // Abort
+    public override void Abort()
+    {
+        performanceUnsaved = Performance;
+        workTimeUnsaved = WorkTime;
+        relaxTimeUnsaved = RelaxTime;
+        base.Abort();
+        UpdateFieldValues();
+        DisactivaveUI();
+    }
+
+    // Change fields
+    public void UpdateFieldValues()
+    {
+        PerformanceField.text = Performance.ToString();
+        WorkField.text = WorkTime.ToString();
+        RelaxField.text = RelaxTime.ToString();
     }
 }
